6bf50684f065b4eb06ab5e8f50961f35443156ba
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-16 23:31:00 +0600

    Call the square function

--- third_python_day.py
+++ third_python_day.py
@@ -8,3 +8,7 @@ def square_number(number):
 
 print("Welcome to Python practice day 3!")
 print("Today I am learning about functions and return values.")
+
+number_to_square = 6
+squared_number = square_number(number_to_square)
+print(f"The square of {number_to_square} is {squared_number}.")
