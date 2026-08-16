538df8f2b5557b236314f0eb7b40ee96313fd0d9
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-16 23:30:48 +0600

    Create a function that squares a number

--- third_python_day.py
+++ third_python_day.py
@@ -1,4 +1,10 @@
 """My third day practicing Python."""
 
+
+def square_number(number):
+    """Return a number multiplied by itself."""
+    return number * number
+
+
 print("Welcome to Python practice day 3!")
 print("Today I am learning about functions and return values.")
