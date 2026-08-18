93d485e655cd99728e3e8fa300590c1053f4b437
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-18 23:38:22 +0600

    Add book details and reading progress

--- fifth_python_day.py
+++ fifth_python_day.py
@@ -4,6 +4,12 @@
 class Book:
     """Represent one book in a reading tracker."""
 
+    def __init__(self, title, author, total_pages):
+        self.title = title
+        self.author = author
+        self.total_pages = total_pages
+        self.pages_read = 0
+
 
 print("Welcome to Python practice day 5!")
 print("Today I am learning the basics of Python classes.")
