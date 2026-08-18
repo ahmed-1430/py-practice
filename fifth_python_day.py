2585ce52b2074d61164cc5b4b54c746211b28294
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-18 23:38:08 +0600

    Create a Book class

--- fifth_python_day.py
+++ fifth_python_day.py
@@ -1,4 +1,9 @@
 """My fifth day practicing Python."""
 
+
+class Book:
+    """Represent one book in a reading tracker."""
+
+
 print("Welcome to Python practice day 5!")
 print("Today I am learning the basics of Python classes.")
