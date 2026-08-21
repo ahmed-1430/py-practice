6fc1d326335801a1253aef3f22584a12633a8012
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-21 23:25:48 +0600

    feat: create person class

--- seventh_python_day.py
+++ seventh_python_day.py
@@ -1,1 +1,18 @@
 """My seventh day practicing Python."""
+
+class Person:
+    """Represent a person."""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        """Introduce the person."""
+        return f"My name is {self.name} and I am {self.age} years old."
+
+
+person = Person("Ahmed", 24)
+
+print("Python Practice Day 7")
+print(person.introduce())
