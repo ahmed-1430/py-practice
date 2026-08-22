6798f6e45df6859dcdb7b629e2f6f86b10e52d9a
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-22 19:00:00 +0600

    feat: create student data

---
+++ eighth_python_day.py
@@ -0,0 +1,22 @@
+"""My eighth day practicing Python."""
+
+students = [
+    {
+        "name": "Ahmed",
+        "age": 24,
+        "student_id": "ST001",
+        "grades": [90, 85, 95],
+    },
+    {
+        "name": "John",
+        "age": 21,
+        "student_id": "ST002",
+        "grades": [70, 65, 75],
+    },
+]
+
+
+print("Python Practice Day 8")
+
+for student in students:
+    print(student)
