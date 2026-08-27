43192888d06a78b6c93d5a715bb6857fc1eae0fb
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-27 23:47:57 +0600

    feat: generate random weather data

--- tenth_python_day.py
+++ tenth_python_day.py
@@ -1,15 +1,27 @@
 """My tenth day practicing Python."""
 
-import math
 import random
+
+
+cities = [
+    "Dhaka",
+    "Chattogram",
+    "Khulna",
+    "Rajshahi",
+    "Sylhet",
+]
+
+
+def get_random_temperature():
+    """Return a random temperature."""
+
+    return random.randint(20, 38)
 
 
 print("Python Practice Day 10")
 
-number = 25
+city = random.choice(cities)
+temperature = get_random_temperature()
 
-print(f"Square root of {number}: {math.sqrt(number)}")
-
-random_number = random.randint(1, 100)
-
-print(f"Random number: {random_number}")
+print(f"City: {city}")
+print(f"Temperature: {temperature}°C")
