4ee7684c27460eac11144d19c02d1ac3c37ecd94
Ahmed <ahmed-1430@users.noreply.github.com>
2026-08-31 23:49:03 +0600

    feat: practice python iterators

---
+++ thirteenth_python_day.py
@@ -0,0 +1,12 @@
+"""My thirteenth day practicing Python."""
+
+
+numbers = [10, 20, 30, 40, 50]
+
+number_iterator = iter(numbers)
+
+print("Python Practice Day 13")
+
+print(next(number_iterator))
+print(next(number_iterator))
+print(next(number_iterator))
